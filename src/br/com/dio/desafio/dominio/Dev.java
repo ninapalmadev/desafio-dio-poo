c178bbfac75101f2496f4bbdd066fca2d49c9b72
Nina Palma <ninapalmadev@gmail.com>
2022-11-09 17:23:20 -0300

    Cria lógica para adicionar dev ao bootcamp e seus conteudos

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,20 +2,32 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
 	private String nome;
 	private Set<Conteudo> conteudoInscritos = new LinkedHashSet<>();
 	private Set<Conteudo> conteudoConcluido = new LinkedHashSet<>();
-	
+
 	public void inscreverBootcamp(Bootcamp bootcamp) {
-		
+		this.conteudoInscritos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsInscritos().add(this);
 	}
-	
-	public void progredir() {}
-	
-	public void calcularTotalXp() {}
+
+	public void progredir() {
+		Optional<Conteudo> conteudo = this.conteudoInscritos.stream().findFirst();
+		if (conteudo.isPresent()) {
+			this.conteudoConcluido.add(conteudo.get());
+			this.conteudoInscritos.remove(conteudo.get());
+		} else {
+			System.err.println("Você não está inscrito em nenhum conteúdo!");
+		}
+	}
+
+	public double calcularTotalXp() {
+		return this.conteudoConcluido.stream().mapToDouble(conteudo -> conteudo.calculaXp()).sum();
+	}
 
 	public String getNome() {
 		return nome;
@@ -58,7 +70,5 @@ public class Dev {
 		return Objects.equals(conteudoConcluido, other.conteudoConcluido)
 				&& Objects.equals(conteudoInscritos, other.conteudoInscritos) && Objects.equals(nome, other.nome);
 	}
-	
-	
-	
+
 }
